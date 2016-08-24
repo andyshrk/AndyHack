8c9626bccf3b9ddb1d9667ea85a8f1f383ae1d0f
Andy <andyshrk@gmail.com>
2016-08-24 18:31:53 +0800

    c_hack: sizeof: sizeof(unsigned long)
    Signed-off-by: Andy <andyshrk@gmail.com>

--- c_hack/sizeof/sizeof.c
+++ c_hack/sizeof/sizeof.c
@@ -4,12 +4,14 @@ int main()
 {
 	int *p = NULL;
 	int a[100];
+	unsigned long phy;
 	printf("sizeof(p) = %ld \n", sizeof(p));
 	printf("sizeof(*p) = %ld \n", sizeof(*p));
 	printf("sizeof(a) = %ld \n", sizeof(a));
 	printf("sizeof(a[100]) = %ld \n", sizeof(a[100]));
 	printf("sizeof(&a) = %ld \n", sizeof(&a));
 	printf("sizeof(&a[0]) = %ld \n", sizeof(&a[0]));
+	printf("sizeof(unsigned long) = %d\n", sizeof(phy));
 	
 	return 0;
 }
