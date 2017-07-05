a3bc3b8890f2395574b9214194b76e7d1b6c5680
Andy Yan <andy.yan@rock-chips.com>
2017-07-05 10:50:27 +0800

    shell: fortest: support multi var loop
    Signed-off-by: Andy Yan <andy.yan@rock-chips.com>

--- shell/fortest.sh
+++ shell/fortest.sh
@@ -1,5 +1,14 @@
-#!/bin/sh
+#!/bin/bash
+DEF="a b c d"
+RES="1 2 3 4"
+index=0
 for i in $(seq $1 $2)
 do
 echo $i
+done
+
+for def in ${DEF};do
+let index++
+res=`echo ${RES} | awk -v idx=${index} '{print $idx}'`
+echo $def $res
 done
