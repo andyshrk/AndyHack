a8f7f46ec82ff356b0b40d276e87b904e3d2914f
Andyshrk <andyshrk@gmail.com>
2020-11-29 08:36:15 +0800

    shell: vop2_dump: Format update
    Signed-off-by: Andyshrk <andyshrk@gmail.com>

--- shell/vop2_dump.sh
+++ shell/vop2_dump.sh
@@ -1,56 +1,79 @@
 #!/system/bin/sh
+echo "SYS:"
 io -r -4 -l 0x100 0xfe040000
 print
+echo "OVL:"
 io -r -4 -l 0x100 0xfe040600
 print
 
 STAT=`cat /sys/kernel/debug/dri/0/summary  | grep "Video Port0" | grep "ACTIVE"`
 if  [ "$STAT" != "" ]; then
-io -r -4 -l 0x100 0xfe040c00
-print
+	echo "VP0:"
+	io -r -4 -l 0x100 0xfe040c00
+	print
 fi
 
 STAT=`cat /sys/kernel/debug/dri/0/summary  | grep "Video Port1" | grep "ACTIVE"`
 if  [ "$STAT" != "" ]; then
-io -r -4 -l 0x100 0xfe040d00
-print
+	echo "VP1:"
+	io -r -4 -l 0x100 0xfe040d00
+	print
 fi
 
 STAT=`cat /sys/kernel/debug/dri/0/summary  | grep "Video Port2" | grep "ACTIVE"`
 if  [ "$STAT" != "" ]; then
-io -r -4 -l 0x100 0xfe040e00
-print
+	echo "VP2:"
+	io -r -4 -l 0x100 0xfe040e00
+	print
 fi
 
 STAT=`cat /sys/kernel/debug/dri/0/summary  | grep "Video Port3" | grep "ACTIVE"`
 if  [ "$STAT" != "" ]; then
-io -r -4 -l 0x100 0xfe040f00
-print
+	echo "VP3:"
+	io -r -4 -l 0x100 0xfe040f00
+	print
 fi
 
-IDS=`cat /sys/kernel/debug/dri/0/summary  | grep win_id | awk '{print $2}' | sort`
+STAT=`cat /sys/kernel/debug/dri/0/summary  | grep "Cluster0-win0" | grep "ACTIVE"`
+if  [ "$STAT" != "" ]; then
+	echo "Cluster0: "
+	io -r -4 -l 0x130 0xfe041000
+	print
+fi
 
-for i in $IDS; do
-    if [ $i = 0 ]; then
-        echo "Cluster0: "
-        io -r -4 -l 0x130 0xfe041000
-    elif [ $i = 1 ]; then
-        echo "Cluster1:"
-        io -r -4 -l 0x130 0xfe041200
-    elif [ $i = 4 ]; then
-        echo "Esmart0: "
-        io -r -4 -l 0x100 0xfe041800
-    elif [ $i = 5 ]; then
-        echo "Esmart1:"
-        io -r -4 -l 0x100 0xfe041a00
-    elif [ $i = 6 ]; then
-        echo "Smart0:"
-        io -r -4 -l 0x100 0xfe041c00
-    elif [ $i = 6 ]; then
-        echo "Smart1:"
-        io -r -4 -l 0x100 0xfe041e00
-    fi
-    print
-done
+STAT=`cat /sys/kernel/debug/dri/0/summary  | grep "Cluster1-win0" | grep "ACTIVE"`
+if  [ "$STAT" != "" ]; then
+	echo "Cluster1: "
+	io -r -4 -l 0x130 0xfe041200
+	print
+fi
+
+STAT=`cat /sys/kernel/debug/dri/0/summary  | grep "Esmart0-win0" | grep "ACTIVE"`
+if  [ "$STAT" != "" ]; then
+	echo "Esmart0: "
+	io -r -4 -l 0x100 0xfe041800
+	print
+fi
+
+STAT=`cat /sys/kernel/debug/dri/0/summary  | grep "Esmart1-win0" | grep "ACTIVE"`
+if  [ "$STAT" != "" ]; then
+	echo "Esmart1: "
+	io -r -4 -l 0x100 0xfe041a00
+	print
+fi
+
+STAT=`cat /sys/kernel/debug/dri/0/summary  | grep "Smart0-win0" | grep "ACTIVE"`
+if  [ "$STAT" != "" ]; then
+	echo "Smart0: "
+	io -r -4 -l 0x100 0xfe041c00
+	print
+fi
+
+STAT=`cat /sys/kernel/debug/dri/0/summary  | grep "Smart1-win0" | grep "ACTIVE"`
+if  [ "$STAT" != "" ]; then
+	echo "Smart1: "
+	io -r -4 -l 0x100 0xfe041e00
+	print
+fi
 
 cat /sys/kernel/debug/dri/0/summary
